93b5ac17b5aca699e94bfaf132e1f3db4d768a26
accfmax95 <98619714+accfmax95@users.noreply.github.com>
2022-11-06 19:15:28 -0700

    Update tummiGUI.py

--- tummiGUI.py
+++ tummiGUI.py
@@ -1,11 +1,15 @@
+#!/usr/bin/env python3
+
+from tkinter import filedialog
 import tkinter as tk
+import os
 
 from PIL import ImageTk, Image
 from tkinter.filedialog import askopenfile
 
 #must enter this command in cmd
 # pip install Pillow
-
+base = False
 root = tk.Tk()
 
 canvas = tk.Canvas(root, width=1000,height=400)
@@ -25,18 +29,35 @@ instructions = tk.Label(root, text="Select an executable on your computer to ana
 instructions.grid(columnspan=3,row=1)
 
 def open_file():
+    global success
+    global base
+    if base:
+        success.grid_forget()
+
     browser_text.set("loading...")
-    file = askopenfile(parent=root,mode='rb',title="Choose a file", filetype=[("Exe file", "*.exe")])
+    file = filedialog.askopenfile(parent=root,mode='rb',title="Choose a file", filetype=[("Exe file", "*.exe")])
     if file:
-        print("file was successfully loaded")
+        file_name = os.path.basename(file.name)
+        name = os.path.splitext(file_name)[0]
+        pack = "UNPACKED"
+        hash = "HASH"
+        file2 = open(f'{pack}-{name}.exe', "x")
+        file2 = open(f'{hash}-{name}.txt', "x")
+
+        # Code for unpacking and writing to the file
+
+        base = True
+        success = tk.Label(root, text="Your unpacked file and hash data has been added to your directory", font="Raleway")
+        success.grid(columnspan=5,row=3)
+        browser_text.set("Browse")
+        return
 
 
 #browser button
 browser_text = tk.StringVar()
 browser_btn = tk.Button(root, textvariable=browser_text, command=lambda:open_file(),font="Raleway", bg="#20bebe", fg="white",height=2,width=15)
-browser_text.set("Browse")
 browser_btn.grid(column=1,row=2)
-
+browser_text.set("Browse")
 canvas = tk.Canvas(root, width=1000,height=200)
 canvas.grid(columnspan=3)
 
